2a9c97e62f7261bbc9ae34e7c5adb3144456f8e6
Georgie Horth <georgie.horth@housekeep.com>
2024-05-08 11:38:46 +0100

    [#187356151] Add `select_for_update` to `lock` method

--- django_cron/backends/lock/database.py
+++ django_cron/backends/lock/database.py
@@ -5,13 +5,13 @@ from django.db import transaction
 
 class DatabaseLock(DjangoCronJobLock):
     """
-    Locking cron jobs with database. Its good when you have not parallel run and want to make sure 2 jobs won't be
+    Locking cron jobs with database. It's good when you have not parallel run and want to make sure 2 jobs won't be
     fired at the same time - which may happened when job execution is longer that job interval.
     """
 
     @transaction.atomic
     def lock(self):
-        lock, created = CronJobLock.objects.get_or_create(job_name=self.job_name)
+        lock, created = CronJobLock.objects.select_for_update().get_or_create(job_name=self.job_name)
         if lock.locked:
             return False
         else:
